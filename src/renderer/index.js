c708e6e56f9a4f196ef2ce32e6d5abdfb2d701c4
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-08-17 07:52:42 +0200

    add image

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,12 +1,12 @@
 import getStatic from "./utils/getStatic";
-
-let cssPath = getStatic("theme.css");
-
+ 
 let linkElt = document.createElement("link");
 linkElt.setAttribute("rel", "stylesheet");
 linkElt.setAttribute("type", "text/css");
-linkElt.setAttribute("href", cssPath);
+linkElt.setAttribute("href", getStatic("theme.css"));
 document.head.appendChild(linkElt);
 
 document.write(`<h1>${require('../../package.json').name}</h1>`)
-document.write(`<div>css path: ${cssPath}</div>`)
+document.write(`<img class="logo" src="${getStatic('electron.png')}" />`)
+document.write(`<p>env: ${process.env.NODE_ENV}</p>`)
+document.write(`<p>static path: ${getStatic('')}</p>`)
