8ff2b6d08e96a94a917378a7922b36d9fd4d1a6f
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-08-17 09:21:58 +0200

    move ts declaration to separate file

--- src/renderer/utils/getStatic.ts
+++ src/renderer/utils/getStatic.ts
@@ -1,8 +1,6 @@
 import path from "path";
 import * as url from "url";
 
-declare const __static:string;
-
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // see https://github.com/electron-userland/electron-webpack/issues/241#issuecomment-582920906
